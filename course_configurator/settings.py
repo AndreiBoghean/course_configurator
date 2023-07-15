ff1825608e8572b3ad5218dfc2be6bfe1baad995
Andrei Boghean <andreiboghean00@gmail.com>
2023-07-16 00:17:01 +0100

    add files for running via docker compose

--- course_configurator/settings.py
+++ course_configurator/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-!$r_cv(61n(a3apb0#r2v^w)zgdyyy=-09y4dzhcae6@7c=%rr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
